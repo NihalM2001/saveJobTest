8c50176f1694a61385d2210c1d7e569b461f85e1
nihal <nihalmurali047@gmail.com>
2024-11-07 12:12:36 +0530

    added the Project file into the Repository.

---
+++ saveJobs/src/main/java/com/project/saveJobs/SaveJobsApplication.java
@@ -0,0 +1,13 @@
+package com.project.saveJobs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaveJobsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaveJobsApplication.class, args);
+	}
+
+}
---
+++ saveJobs/src/test/java/com/project/saveJobs/SaveJobsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.saveJobs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaveJobsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
