43529be02eb0ff5c55681371dc5f3b633048dce9
nihal <nihalmurali047@gmail.com>
2024-11-08 14:06:39 +0530

    Entered the Logic for listing all the job entered by the User.

--- saveJobs/src/main/java/com/project/saveJobs/service/JobService.java
+++ saveJobs/src/main/java/com/project/saveJobs/service/JobService.java
@@ -6,6 +6,8 @@ import lombok.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @AllArgsConstructor
 @NoArgsConstructor
@@ -20,5 +22,10 @@ public class JobService {
     public JobModel saveJob(JobModel jm)
     {
         return jobRepo.save(jm);
+    }
+
+    public List<JobModel> listAllJobs()
+    {
+        return jobRepo.findAll();
     }
 }
