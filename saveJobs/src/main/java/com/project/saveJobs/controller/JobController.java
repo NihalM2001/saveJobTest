d6057e804abdae7c9b13de7b8be57df80a093977
nihal <nihalmurali047@gmail.com>
2024-11-08 13:06:10 +0530

    Configured the controller class for saving the job details entered by the User. Test running the application by API testing using Postman

--- saveJobs/src/main/java/com/project/saveJobs/controller/JobController.java
+++ saveJobs/src/main/java/com/project/saveJobs/controller/JobController.java
@@ -1,4 +1,20 @@
 package com.project.saveJobs.controller;
 
+import com.project.saveJobs.model.JobModel;
+import com.project.saveJobs.service.JobService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class JobController {
+    @Autowired
+    JobService jobSer;
+
+    @PostMapping("/saveJob")
+    public JobModel saveJob(@RequestBody JobModel jm)
+    {
+        return jobSer.saveJob(jm);
+    }
 }
