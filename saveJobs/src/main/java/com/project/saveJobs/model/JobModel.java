9a4fbb54025a61c7342bbe31161b41491c41b537
nihal <nihalmurali047@gmail.com>
2024-11-08 11:53:53 +0530

    Configured the model file and the application.properties file and the table has been automatically added to the database.

--- saveJobs/src/main/java/com/project/saveJobs/model/JobModel.java
+++ saveJobs/src/main/java/com/project/saveJobs/model/JobModel.java
@@ -1,4 +1,25 @@
 package com.project.saveJobs.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "JobDetails")
 public class JobModel {
+    @Id
+    @GeneratedValue
+    private long sNo;
+    private String companyName;
+    private String appliedDate;
+    private String callback;
+    private String interview;
+    private String selected;
 }
