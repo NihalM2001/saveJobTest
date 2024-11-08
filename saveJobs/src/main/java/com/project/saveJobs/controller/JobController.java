19e4ddf5a96ad35016f050b8531e8c9cb7ae8824
nihal <nihalmurali047@gmail.com>
2024-11-08 14:07:50 +0530

    Configured the controller to list all the jobs

--- saveJobs/src/main/java/com/project/saveJobs/controller/JobController.java
+++ saveJobs/src/main/java/com/project/saveJobs/controller/JobController.java
@@ -3,9 +3,12 @@ package com.project.saveJobs.controller;
 import com.project.saveJobs.model.JobModel;
 import com.project.saveJobs.service.JobService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class JobController {
@@ -16,5 +19,11 @@ public class JobController {
     public JobModel saveJob(@RequestBody JobModel jm)
     {
         return jobSer.saveJob(jm);
+    }
+
+    @GetMapping("/listJob")
+    public List<JobModel> listAllJob()
+    {
+        return jobSer.listAllJobs();
     }
 }
