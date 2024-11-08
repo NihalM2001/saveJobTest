b28146c5bcb9de7a459e2c1b7da86310500b35e5
nihal <nihalmurali047@gmail.com>
2024-11-08 12:55:25 +0530

    Added the logic for saving the job details in the service file. Over to create the controller class to save the details

--- saveJobs/src/main/java/com/project/saveJobs/service/JobService.java
+++ saveJobs/src/main/java/com/project/saveJobs/service/JobService.java
@@ -1,4 +1,24 @@
 package com.project.saveJobs.service;
 
+import com.project.saveJobs.model.JobModel;
+import com.project.saveJobs.repository.JobRepository;
+import lombok.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@Data
 public class JobService {
+    @Autowired
+    JobRepository jobRepo;
+
+    public JobModel saveJob(JobModel jm)
+    {
+        return jobRepo.save(jm);
+    }
 }
